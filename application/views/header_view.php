c8763df0ba0b5f74ea9772b3e82c47a2fbf3b4f1
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2023-11-08 01:27:24 +0500

    url update

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -12,8 +12,26 @@
 			           <li class="nav-item active">
                             <a class="nav-link" href="<?=base_url();?>">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?=base_url('our-services');?>">Services</a>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Services
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="servicesDropdown">
+                            <a class="dropdown-item" href="<?= base_url('our-services'); ?>">Our Services</a>
+                            <a class="dropdown-item" href="<?= base_url('services/web-development'); ?>">Web Development</a>
+                            <a class="dropdown-item" href="<?= base_url('services/app-development'); ?>">App Development</a>
+                            <a class="dropdown-item" href="<?= base_url('services/social-media'); ?>">Social Media</a>
+                            <a class="dropdown-item" href="<?= base_url('services/seo-optimization'); ?>">Seo Otimization</a>
+                        </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="portfolioDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Portfolio
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="portfolioDropdown">
+                            <a class="dropdown-item" href="<?= base_url('projects/project-list'); ?>">Project List</a>
+                            <a class="dropdown-item" href="<?= base_url('projects/gig-qatar'); ?>">Gig Qatar project</a>
+                        </div>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#clients">Clients</a>
